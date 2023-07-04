6d20f5b38772687c701842d1cb28ffa8df833770
lace04 <luisace04@gmail.com>
2023-07-04 15:38:04 -0500

    setup clerk

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs';
+
+const SetupPage = () => {
+  return (
+    <>
+      <div className='p-4'>
+        <UserButton afterSignOutUrl='/' />
+      </div>
+    </>
+  );
+};
+
+export default SetupPage;
---
+++ ecommerce-admin/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <>
+      <p>Portal</p>
+      <div className='p-4'>
+        <Button>
+          Boton
+        </Button>
+      </div>
+    </>
+  );
+}
