062ca9f75bd2d65c3df4311fc3f41bacd26af1d5
lace04 <luisace04@gmail.com>
2023-07-08 17:48:17 -0500

    Form Component tsx

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,12 +1,23 @@
-import { UserButton } from '@clerk/nextjs';
+'use client';
+
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
 
 const SetupPage = () => {
+
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
-    <>
-      <div className='p-4'>
-        <UserButton afterSignOutUrl='/' />
-      </div>
-    </>
+    <div className='p-4'>
+      Root Page
+    </div>
   );
 };
 
